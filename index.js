b255db11caef471227610087b622525520b75d76
SebaGut2103 <juansefernando.guti2103@gmail.com>
2025-01-09 21:10:15 -0500

    terminar

---
+++ index.js
@@ -0,0 +1,9 @@
+const texto = document.getElementById("texto");
+async function info(){
+    try{
+        const data = await fetch("https://api-colombia.com/swagger/index.html")
+        if(data.ok){
+            
+        }
+    }
+} 
